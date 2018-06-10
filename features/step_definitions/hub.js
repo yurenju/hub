1c5e05da8682104f1014005aa86f5c8e2dd8a240
Yuren Ju <yurenju@gmail.com>
2018-06-10 22:03:38 +0800

    Added first scenario

---
+++ features/step_definitions/hub.js
@@ -0,0 +1,21 @@
+const { Given, When, Then } = require('cucumber');
+
+Given('hub contract is deployed', function() {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+When('set ticket sales fee to {float}%', function(int) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+When('a host create an event with ticket price {float} ETH', function(int) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+Then('service fee per ticket is {float} ETH', function(float) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
